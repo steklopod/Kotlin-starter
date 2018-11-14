06f00d5592f99ca1bd2a1a24b8dcbb0e627c1fa4
Dima Koltovich <steklopod@gmail.com>
2018-11-14 10:39:33 +0300

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.3.10"
+    id("org.springframework.boot") version "2.0.5.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+    id("io.spring.dependency-management") version "1.0.5.RELEASE"
+}
+
+version = "1.0.0-SNAPSHOT"
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.springframework.boot:spring-boot-starter-data-jpa")
+    compile("com.h2database:h2")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compile("org.jetbrains.kotlin:kotlin-reflect")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testCompile("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
---
+++ src/main/kotlin/ru/steklopod/CustomerController.kt
@@ -0,0 +1,16 @@
+package ru.steklopod
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController(private val repository: CustomerRepository) {
+
+	@GetMapping("/customers")
+	fun findAll() = repository.findAll()
+
+	@GetMapping("/customers/{lastName}")
+	fun findByLastName(@PathVariable lastName:String)
+			= repository.findByLastName(lastName)
+}
