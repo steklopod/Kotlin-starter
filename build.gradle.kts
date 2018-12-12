a70b5c46d47bf188e75d3dcfb35d2fc2f7a5df80
Колтович Дмитрий <dkoltovich@roox.ru>
2018-12-12 14:51:33 +0300

    Build gradle updated - 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
+import org.gradle.internal.impldep.org.junit.platform.launcher.EngineFilter.includeEngines
 
 plugins {
     val kotlinVersion = "1.3.10"
-
+    base
+    java
+    application
     id("org.springframework.boot") version "2.1.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-//    id("org.jetbrains.kotlin.jvm") version kotlinVersion
 
+}
+application {
+    mainClassName = "ru.Application.kt"
 }
 
 group = "ru.steklopod"
@@ -17,16 +25,17 @@ repositories {
     mavenLocal()
     mavenCentral()
     jcenter()
+    gradlePluginPortal()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web") { exclude(module = "spring-boot-starter-tomcat") }
     implementation("org.springframework.boot:spring-boot-starter-jetty")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("com.h2database:h2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") { exclude(module = "junit") }
     testImplementation("org.junit.jupiter:junit-jupiter-api")
@@ -36,10 +45,13 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-databind")
+
     runtime("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
     runtime("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     runtime("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtime("com.h2database:h2:1.4.197")
 }
+
 
 tasks {
     withType<KotlinCompile> {
@@ -48,7 +60,7 @@ tasks {
             freeCompilerArgs = listOf("-Xjsr305=strict")
         }
     }
-    withType(Test::class) {
+    getByName<Test>("test") {
         useJUnitPlatform {
             includeEngines("junit-jupiter")
             excludeEngines("junit-vintage")
--- src/test/kotlin/ru/steklopod/ApplicationTests.kt
+++ src/test/kotlin/ru/steklopod/ApplicationTests.kt
@@ -11,13 +11,16 @@ import org.springframework.boot.test.web.client.getForObject
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationTests(@Autowired private val restTemplate: TestRestTemplate) {
 
+    companion object {
+        val content =
+            """[{"firstName":"Jack","lastName":"Bauer","id":1},{"firstName":"Chloe","lastName":"O'Brian","id":2},{"firstName":"Kim","lastName":"Bauer","id":3},{"firstName":"David","lastName":"Palmer","id":4},{"firstName":"Michelle","lastName":"Dessler","id":5}]"""
+    }
     @Test
     fun findAll() {
-        val content = """[{"firstName":"Jack","lastName":"Bauer","id":1},{"firstName":"Chloe","lastName":"O'Brian","id":2},{"firstName":"Kim","lastName":"Bauer","id":3},{"firstName":"David","lastName":"Palmer","id":4},{"firstName":"Michelle","lastName":"Dessler","id":5}]"""
-        assertEquals(content, restTemplate.getForObject<String>("/customers"))
+         assertEquals(content, restTemplate.getForObject<String>("/customers"))
     }
 
 }
